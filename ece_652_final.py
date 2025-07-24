a19e0e6615e72a537a08ce9e5fb8630177253419
Ahmad Bilal Irfan <bilal99u@gmail.com>
2025-07-24 19:07:37 -0400

    Running reading in while loop until correct file name inputed

--- ece_652_final.py
+++ ece_652_final.py
@@ -2,17 +2,23 @@ import os
 
 print("Welcome to Ahmad Bilal Irfan's RMS Simulator")
 
-workLoadFileName = input("Enter the workload file name: ")
-print("You entered: " + workLoadFileName)
-print(type(workLoadFileName))
+workLoadFileName = "" # Initialize the workload file name
+content = "" # Initialize content variable, which will hold the file content
 
-# Check if the file exists
-if os.path.isfile(workLoadFileName):
-    print(f"File '{workLoadFileName}' exists. Opening in read mode...")
-    with open(workLoadFileName, 'r') as file:
-        content = file.read()
-        print("File content:")
-        print(content)
-        # You can add parsing logic here
-else:
-    print(f"File '{workLoadFileName}' does not exist.")
+while (True):
+    workLoadFileName = input("Enter the workload file name: ")
+    print("You entered: " + workLoadFileName)
+
+    # Check if the file exists
+    if os.path.isfile(workLoadFileName):
+        print(f"File '{workLoadFileName}' exists. Opening in read mode...")
+        with open(workLoadFileName, 'r') as file:
+            content = file.read()
+            print("File content:")
+            print(content)
+            break
+            # You can add parsing logic here
+    else:
+        print(f"File '{workLoadFileName}' does not exist, Please enter a valid file name.")
+
+print ("File was read successfully.")
